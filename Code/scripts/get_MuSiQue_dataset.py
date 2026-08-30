f284787c688c1455410ad89fa8dbd397a59920cb
Jschechter18 <joshua.d.schechter@gmail.com>
2026-08-30 14:11:13 -0400

    added validation download to the script

--- Code/scripts/get_MuSiQue_dataset.py
+++ Code/scripts/get_MuSiQue_dataset.py
@@ -4,10 +4,14 @@ from pathlib import Path
 dataset = load_dataset("dgslibisey/MuSiQue")
 
 train = dataset["train"]
+validation = dataset["validation"]
 
 output_dir = Path("data/MuSiQue/clean")
 output_dir.mkdir(parents=True, exist_ok=True)
 
 train_file = output_dir / "train.json"
+validation_file = output_dir / "validation.json"
+
 
 train.to_json(train_file)
+validation.to_json(validation_file)
