6b46aa7a20fed18b2952dfcdb73028f693fc90e6
Jschechter18 <joshua.d.schechter@gmail.com>
2026-08-30 14:12:34 -0400

    saving dataset script

--- Code/scripts/get_MuSiQue_dataset.py
+++ Code/scripts/get_MuSiQue_dataset.py
@@ -1,17 +1,18 @@
 from datasets import load_dataset
 from pathlib import Path
 
-dataset = load_dataset("dgslibisey/MuSiQue")
+if __name__ == "__main__":
+    dataset = load_dataset("dgslibisey/MuSiQue")
 
-train = dataset["train"]
-validation = dataset["validation"]
+    train = dataset["train"]
+    validation = dataset["validation"]
 
-output_dir = Path("data/MuSiQue/clean")
-output_dir.mkdir(parents=True, exist_ok=True)
+    output_dir = Path("data/MuSiQue/clean")
+    output_dir.mkdir(parents=True, exist_ok=True)
 
-train_file = output_dir / "train.json"
-validation_file = output_dir / "validation.json"
+    train_file = output_dir / "train.json"
+    validation_file = output_dir / "validation.json"
 
 
-train.to_json(train_file)
-validation.to_json(validation_file)
+    train.to_json(train_file)
+    validation.to_json(validation_file)
